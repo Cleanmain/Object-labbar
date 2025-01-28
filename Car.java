9dc0e435be9ce0c8ed210f1aa7e560fa06c50c18
Filip Helin <tabbekelin@gmail.com>
2025-01-28 20:55:50 +0100

    Klar med del 2

--- Car.java
+++ Car.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public abstract class Car {
+public abstract class Car implements Movable{
 
     private Color color;
     private int nrDoors;
@@ -8,12 +8,23 @@ public abstract class Car {
     private double enginePower;
     private String modelName;
 
+    private double x;
+    private double y;
+    private Direction direction;
+
+    public enum Direction{     //"Låser" ner vad vi kan skriva som direction
+        North, East, South, West;
+    }
+
     public Car(Color color, int nrDoors, double enginePower, String modelName) {
         this.color = color;
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
         this.modelName = modelName;
         stopEngine();
+        this.x = 0;
+        this.y = 0;
+        this.direction = Direction.North;    //Börjar mot norr
 
     }
 
@@ -50,6 +61,47 @@ public abstract class Car {
     public abstract void incrementSpeed(double amount);
     public abstract void decrementSpeed(double amount);
 
+    @Override
+    public void move() {
+        if (direction == Direction.North) {
+            y += currentSpeed;
+        } else if (direction == Direction.South) {
+            y -= currentSpeed;
+        } else if (direction == Direction.East) {
+            x += currentSpeed;
+        } else if (direction == Direction.West) {
+            x -= currentSpeed;
+        }
+    }
+
+    public void turnLeft() {
+        if (direction == Direction.North) {
+            direction = Direction.West;
+        } else if (direction == Direction.West) {
+            direction = Direction.South;
+        } else if (direction == Direction.South) {
+            direction = Direction.East;
+        } else {
+            direction = Direction.North;
+        }
+    }
+
+    @Override
+    public void turnRight() {
+        if (direction == Direction.North) {
+            direction = Direction.East;
+        } else if (direction == Direction.East) {
+            direction = Direction.South;
+        } else if (direction == Direction.South) {
+            direction = Direction.West;
+        } else {
+            direction = Direction.North;
+        }
+    }
+
+    public String getPosition() {
+        return "(" + x + ", " + y + ")";
+    }
 
 }
 // Båda bilar Saab och volvo är tydligt "relaterade" med Bilar som innebär att ärv är perfect
